1f05d62b8ef55e249ad7a64db7be6d249c4dfa12
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-19 18:00:29 +0200

    added user item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { FirstcompComponent } from './firstcomp/firstcomp.component';
+import { UserItemComponent } from './user-item/user-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FirstcompComponent
+    FirstcompComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule,
