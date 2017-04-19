03b2412aec1c739b9de1afc2044d4dbda6aea29c
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-04-19 17:57:41 +0200

    add a component ng generate component firstcomp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { FirstcompComponent } from './firstcomp/firstcomp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstcompComponent
   ],
   imports: [
     BrowserModule,
