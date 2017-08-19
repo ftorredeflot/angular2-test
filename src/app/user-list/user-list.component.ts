c294c4fe71f583fe6a95d5566df1f02a12edd4e9
Ferran Torredeflot <ftorredeflot@gmail.com>
2017-08-19 20:16:42 +0200

    provant pippes i creant funcio

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -7,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListComponent implements OnInit {
  names:string[];
+ prices;
+  totalpreu(){
+   let sum=0;
+   for(let price of this.prices){
+     sum +=price.precio;
+   }
+   return sum;
+  }
   constructor() {
     this.names=["ricard", "paco", "llauna", "nosequeponer"]
+    this.prices=[{"precio":4.20,"patata":"TRUE"},{"precio":30,"patata":"TRUE"},{"precio":50,"patata":"TRUE"}]
+
   }
 
   ngOnInit() {
